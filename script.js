8564f092e833aea92d25f0f865ab2670f9653dd9
Alexandre Francisco <alexmvfrancisco@gmail.com>
2024-02-29 19:27:38 +0000

    front page updated
    front page updated before meeting

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        var title = document.querySelector('.title');
+        title.style.opacity = 1;
+        title.style.transform = 'translateX(0)';
+    }, 1500);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
+        var text = document.querySelector('.descriptive-text');
+        text.style.opacity = 1;
+        text.style.transform = 'translateX(0)';
+    }, 1900);
+});
